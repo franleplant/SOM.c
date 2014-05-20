3538d712764ea3fc21238af749104bb791b2be37
franleplant <franleplant@gmail.com>
2014-05-20 20:51:56 -0300

    first full sphere generation code. A lot of improvements to do

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h> /* required for randomize() and random() */
 #include <math.h> /*This is needed for the sqrt and pow functions to work*/
 
-#define MAX_NEURON_NUMBER 10
-
+#define MAX_NEURON_NUMBER 270
+#define MAX_ITERATION_NUMBER 3000
 
 
 
@@ -24,20 +24,66 @@ double calc_norm(Point p);
 
 main() {
 
-	Point Sphere[MAX_NEURON_NUMBER];
+	Point Neuron[MAX_NEURON_NUMBER];
 
 	int i;
-
-	for(i = 0; i < MAX_NEURON_NUMBER; i++) {
-		standarize(  random_point(&Sphere[i])  );
-		printf("(%f, %f, %f) norm = %f \n", Sphere[i].x, Sphere[i].y, Sphere[i].z, calc_norm(Sphere[i]));
-		
-	}
+	int iter;
+	int j;
+	int k;
+	Point fer_i;
+	Point distance;
+	double distance_norm;
+	float dltt = 0.1;
 
 	//initializice vector
+	for(i = 0; i < MAX_NEURON_NUMBER; i++) {
+		standarize(  random_point(&Neuron[i])  );
+		//printf("(%f, %f, %f) norm = %f \n", Neuron[i].x, Neuron[i].y, Neuron[i].z, calc_norm(Neuron[i]));
+		
+	};
 
-    printf("Hello World");
 
+	//Calculate the neuron distribution
+	for(iter = 0; iter < MAX_ITERATION_NUMBER; iter++){
+
+
+		for(i = 0; i < MAX_NEURON_NUMBER; i++) {
+
+
+			fer_i.x = 0;
+			fer_i.y = 0;
+			fer_i.z = 0;
+
+			for(j = 0; j < MAX_NEURON_NUMBER; j++) {
+
+				distance.x = Neuron[i].x - Neuron[j].x;
+				distance.y = Neuron[i].y - Neuron[j].y;
+				distance.z = Neuron[i].z - Neuron[j].z;
+
+				distance_norm = calc_norm(distance);
+
+				if (distance_norm > 0) {
+
+					fer_i.x += distance.x / distance_norm;
+					fer_i.y += distance.y / distance_norm;
+					fer_i.z += distance.z / distance_norm;
+				}
+			};
+
+			//Calculate the position change due to the Electrostatic Repulsion Force aceleration
+			Neuron[i].x += fer_i.x * pow(dltt, 2);
+			Neuron[i].y += fer_i.y * pow(dltt, 2);
+			Neuron[i].z += fer_i.z * pow(dltt, 2);
+
+			standarize(  &Neuron[i]  );
+
+			printf("(%f, %f, %f) it = %d \n", Neuron[i].x, Neuron[i].y, Neuron[i].z, iter);
+
+		}
+
+	}
+
+	
 };
 
 
@@ -61,6 +107,7 @@ Point * random_point(Point *p) {
 
 
 double calc_norm(Point p) {
+
 	return sqrt(  pow(p.x, 2) + pow(p.y, 2) + pow(p.z, 2) ); 
 };
 
